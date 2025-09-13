9782417ee5fca19cc394949c7502371ea3872f71
Aw Man <brambilladaniele@icloud.com>
2025-09-13 22:20:52 +0200

    refactoring

--- RSN_request_processors.lua
+++ RSN_request_processors.lua
@@ -6,6 +6,24 @@ local METHODS = {
 }
 
 
+-- FUNCTIONS
+
+local function readFile(path)
+    if not fs.exists(path) then return nil end
+    local fh = fs.open(path, 'r')
+    local content = fh.readAll()
+    fh.close()
+    return content
+end
+
+local function writeFile(path, content)
+    local fh = fs.open(path, 'w')
+    fh.write(content)
+    fh.close()
+    return true
+end
+
+
 -- PREDEFINED REQUEST PROCESSORS
 
 local requestProcessors = {} -- table to hold them
@@ -14,26 +32,22 @@ function requestProcessors.static(directory)
     return function (senderId, method, request, protocol)
         if method == METHODS.GET then
             local path = fs.combine(directory, request.path)
-            if not fs.exists(path) then return 404, '"./'..path..'" not found.' end
-            local fh = fs.open(path, 'r')
-            local content = fh.readAll()
-            fh.close()
+            if not fs.exists(path) then return 404, 'Not found' end
+            local content = readFile(path)
             return 100, content
         elseif method == METHODS.PUT then
             local path = fs.combine(directory, request.path)
-            local fh = fs.open(path, 'w')
-            fh.write(request.body)
-            fh.close()
+            local success = writeFile(path, request.body)
             return 100, nil
         else
-            return 201, '"'..method..'" is not a valid method.'
+            return 201, '"'..method..'" is not a valid method'
         end
     end
 end
 
 function requestProcessors.echo()
     return function (senderId, method, request, protocol)
-        return 100, request
+        return 100, request.body
     end
 end
 
